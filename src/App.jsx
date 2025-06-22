69bc3ba0a5788c3b7ec814c1d9a7f2539105d7f0
Capretta1 <blessnutsua3@gmail.com>
2025-06-22 19:07:07 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="bg-green-500 text-3xl">A bg Changer App</h1>
+    <div className="w-full h-screen duration-200">
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full shadow-lg text-black">
+            test
+          </button>
+          <button className="outline-none px-4 py-1 rounded-full shadow-lg text-black">
+            test 2
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
